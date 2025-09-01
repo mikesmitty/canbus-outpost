35d93e993fa1c1821139c826e4c87451ff0ce4ea
Michael Smith <mikejsmitty@gmail.com>
2025-09-01 18:08:48 -0400

    feat: initial commit

---
+++ hardware/footprint.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+REPO_DIR="$(git rev-parse --show-toplevel)"
+PROJECT=$1
+LCSC_ID=$2
+
+if [ -z "$LCSC_ID" ]; then
+    echo "Usage: $0 <project> <lcsc_id>"
+    exit 1
+fi
+
+if [ ! -d "${REPO_DIR}/hardware/${PROJECT}" ]; then
+    echo "Project $PROJECT does not exist"
+    exit 1
+fi
+
+cd "${REPO_DIR}/hardware/${PROJECT}"
+easyeda2kicad --full --project-relative --output ./Library --lcsc_id $LCSC_ID
